431529f6e99dabfb0aa7f0563afabcb1a7cb26ad
Anish Khadka <akhadka2@sfsu.edu>
2023-05-19 12:38:37 -0700

    Update vecadd_gpu_256t_manyblocks.cu

--- vecadd_gpu_256t_manyblocks.cu
+++ vecadd_gpu_256t_manyblocks.cu
@@ -31,6 +31,8 @@ int main(void)
   int numBlocks = (N + blockSize - 1) / blockSize;
   add<<<numBlocks, blockSize>>>(N, x, y);
 
+  std::cout << "Number of thread blocks: " << numBlocks << std::endl;
+
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
 
